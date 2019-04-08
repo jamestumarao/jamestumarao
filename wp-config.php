fa82f60e340c76a3486bce7a926c478ca9db45a1
james tumarao <florencetumarao14@gmail.com>
2019-04-09 03:26:25 +0800

    xD
    xD

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'jamestumarao_db' );
+define( 'DB_NAME', 'jtxga_db' );
 
 /** MySQL database username */
 define( 'DB_USER', 'root' );
@@ -46,14 +46,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'qE}<4(1#Gal-]n;J[wOl[oIzBS;M-m>~JPV,>SXjT3ka$wp_`Be=hpQd`Df``)oM' );
-define( 'SECURE_AUTH_KEY',  'F|2N;GhJ0*T5zd6j?(>!^M[7%&~IA@_UwvnOAg42fvN6CVC;tBh<JTOK=5S!Y*Em' );
-define( 'LOGGED_IN_KEY',    'i2X`)?u32`s5Rf^o jf7%J7_@g:UlL4de?0&*(yP:Se(`olFHu:*0h)-4gM@,/7T' );
-define( 'NONCE_KEY',        'K%DkuA9*S5^*dN2-G)m51piLG`v4tZe/.2B{Bm4W GxxCGu7Z7;=-8j.K I?bG6X' );
-define( 'AUTH_SALT',        'K?fyIo@2J9K|o+o0Hw]6576bYp,O&CvwUM+:x g?aah|AXNybtTxScISFrw^1j_n' );
-define( 'SECURE_AUTH_SALT', '*jW}M.nL3p#sfzXfb/+QnGi;Z;3T6F_:>P`fSHRgYDx4be|y!|5*dM6X]aRnkRQ?' );
-define( 'LOGGED_IN_SALT',   'e3vc;m,zzz9hb 6;X[!+GM/[,q|?>BM?rVrLte-.s(K*Zs3CLCh/W,uM=U40b[h}' );
-define( 'NONCE_SALT',       '!cPL~TuQDL<djJI5 V_;g}Y{).UM>hULiRx`CUw(@$hfGvPA,C{4r:rI/!;2F*4j' );
+define( 'AUTH_KEY',         'RqCCscTrv[#*?ykL)I$Cv&XQ&<`)aAW4hH!)i`)vSVW)$Qq|{:l$Jq<(.-$+,za&' );
+define( 'SECURE_AUTH_KEY',  ']tP~>+>BUi**AGp`06F2~~_GSN{ryY|J<<0?Wwwd5VuKY!`6{prhl`7}=|l95;(d' );
+define( 'LOGGED_IN_KEY',    'pDyz{@V!gx}3_G4;}$)%;_2+l0kTHT&Bc{!,:n~c6M;`IOE_VvZ{pX<p^#dW5xm9' );
+define( 'NONCE_KEY',        'yXU6(/ZFl&_yULEZU1`Zz~O(Io;o`HV2]/1qbF8rdB#~g@+3[0<JYThIT:sE$it@' );
+define( 'AUTH_SALT',        'uZZ 3rvsmwj96l$_^r,BbR*c(Mjp.YwJCO=k b11Zba_s3bMBi=<a8SP`N2E5koz' );
+define( 'SECURE_AUTH_SALT', 'wiX@~Jn-v&XM]2,G;.xfk2Pb)]Os$YDtCUOSsx|[Y3]EoJE%%2cF!3k:3zNMan_:' );
+define( 'LOGGED_IN_SALT',   'Fae?2ku/FBC]#nk&%s{0DlG=o|d@7?kS+4V{2^SL|8F3`!V,>CB4*U1^dKWR@g1{' );
+define( 'NONCE_SALT',       't#3N~.Q<A0>fbX%-QPqOF3|kw;:c83P$!^,<pv{]Li}2~o-o8u,J_|]$}B|N<Sp@' );
 
 /**#@-*/
 
