83bf99662d616afab38004d804e3ac921f6aac9a
james tumarao <florencetumarao14@gmail.com>
2019-04-09 03:27:27 +0800

    x
    d

--- wp-content/plugins/brizy/admin/views/dashboard-news.php
+++
@@ -1,2 +0,0 @@
-<h3><b><?php  echo __('News and Updates')?></b></h3>
-<p><?php  echo __('No News')?></p>
--- wp-content/plugins/brizy/admin/views/dashboard.php
+++
@@ -1,8 +0,0 @@
-<!--<div class="brizy-news">
-    <?php /*echo $news*/ ?>
-</div>
-<hr>
--->
-<div class="brizy-posts">
-	<?php echo $posts ?>
-</div>
